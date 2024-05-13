96cf8fe081fe7ab4de3b70dd627b75172ec1630e
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2024-05-13 14:27:14 -0400

    Initial commit

---
+++ src/test/java/ryhor/mudrahel/textconverter/TextConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package ryhor.mudrahel.textconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
